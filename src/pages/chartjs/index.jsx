ad601377f5f98cfebc5f382c48e98c5f441c8bf1
Daisy <dmduyen1812@gmail.com>
2022-09-21 23:44:47 +0700

    add time scale

--- src/pages/chartjs/index.jsx
+++ src/pages/chartjs/index.jsx
@@ -12,8 +12,11 @@ import {
   LineController,
   BarController,
   Title,
+  TimeScale,
 } from "chart.js";
 import "./index.scss";
+import moment from "moment/moment";
+import "chartjs-adapter-moment";
 
 ChartJS.register(
   LinearScale,
@@ -25,32 +28,140 @@ ChartJS.register(
   Tooltip,
   LineController,
   BarController,
-  Title
+  Title,
+  TimeScale
 );
 
+function newDate(days) {
+  return moment().add(days, "d").toDate();
+}
+
 const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
   datasets: [
     {
       type: "line",
       label: "Social Media",
       borderColor: "rgb(104,228,151)",
       borderWidth: 4,
+      backgroundColor: "rgb(104,228,151)",
       fill: true,
-      data: [23, 42, 35, 27, 43, 22, 17],
+      borderSkipped: "center",
+      data: [
+        {
+          x: new Date("2019-12-31"),
+          y: 23,
+        },
+        {
+          x: new Date("2020-01-01"),
+          y: 42,
+        },
+        {
+          x: new Date("2020-01-02"),
+          y: 35,
+        },
+        {
+          x: new Date("2020-01-03"),
+          y: 27,
+        },
+        {
+          x: new Date("2020-01-04"),
+          y: 43,
+        },
+        {
+          x: new Date("2020-01-05"),
+          y: 22,
+        },
+        {
+          x: new Date("2020-01-06"),
+          y: 17,
+        },
+        {
+          x: new Date("2020-01-07"),
+          y: 31,
+        },
+        {
+          x: new Date("2020-01-08"),
+          y: 22,
+        },
+        {
+          x: new Date("2020-01-09"),
+          y: 22,
+        },
+        {
+          x: new Date("2020-01-10"),
+          y: 12,
+        },
+        {
+          x: new Date("2020-01-11"),
+          y: 16,
+        },
+      ],
       yAxisID: "social",
-      legend: {
-
-      }
+      legend: {},
     },
     {
       type: "bar",
       label: "Website Blog",
+      borderColor: "rgb(40,160,251)",
+      borderWidth: 4,
       backgroundColor: "rgb(40,160,251)",
-      data: [440, 505, 414, 671, 227, 413, 201],
+      fill: true,
+      data: [
+        {
+          x: new Date("2019-12-31"),
+          y: 440,
+        },
+        {
+          x: new Date("2020-01-01"),
+          y: 505,
+        },
+        {
+          x: new Date("2020-01-02"),
+          y: 414,
+        },
+        {
+          x: new Date("2020-01-03"),
+          y: 671,
+        },
+        {
+          x: new Date("2020-01-04"),
+          y: 227,
+        },
+        {
+          x: new Date("2020-01-05"),
+          y: 413,
+        },
+        {
+          x: new Date("2020-01-06"),
+          y: 201,
+        },
+        {
+          x: new Date("2020-01-07"),
+          y: 352,
+        },
+        {
+          x: new Date("2020-01-08"),
+          y: 752,
+        },
+        {
+          x: new Date("2020-01-09"),
+          y: 320,
+        },
+        {
+          x: new Date("2020-01-10"),
+          y: 257,
+        },
+        {
+          x: new Date("2020-01-11"),
+          y: 160,
+        },
+      ],
       borderColor: "white",
       borderWidth: 2,
       yAxisID: "blog",
+      borderSkipped: "center",
+      categoryPercentage: 0.5,
+      barPercentage: 1,
     },
   ],
 };
@@ -78,6 +189,26 @@ const options = {
     mode: "point",
   },
   scales: {
+    x: {
+      type: "time",
+      time: {
+        unit: "day",
+        tooltipFormat: "DD MMM",
+        displayFormats: {
+          day: "DD MMM",
+        },
+      },
+      offset: true,
+      grid: {
+        tickColor: "#90A4AE",
+        tickWidth: 2,
+        borderWidth: 2,
+      },
+      ticks: {
+        align: 'start',
+        labelOffset: 40
+      },
+    },
     blog: {
       type: "linear",
       display: true,
